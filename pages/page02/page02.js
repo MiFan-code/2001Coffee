cafbf9f755edc00d3c23d3f33de6001675881b5b
Alex <q13309815535@163.com>
2021-04-08 21:08:50 +0800

    积分页面

--- pages/page02/page02.js
+++ pages/page02/page02.js
@@ -5,9 +5,77 @@ Page({
    * 页面的初始数据
    */
   data: {
+    clock_button_flag:true,
+    point:2000,
+    count:'0',
+    coins:5,
+  },
+  onBindTap:function(){
+    var D=(new Date()).getDate().toString();
+    if(D != wx.getStorageSync('D')){
+      wx.setStorageSync('D', D);
+      wx.setStorage({
+        key: 'FirstTime',
+        data: (parseInt(this.data.count) + 1).toString(),
+      })
+      var that = this;
+      var count = wx.getStorage({
+        key: 'FirstTime',
+        success: function (res) {     
+          that.setData({
+            count: res.data,
+            clock_button_flag:false,
+          })
+          var count_int = parseInt(that.data.count)
+          var temp = that.data.point
+          temp += count_int * that.data.coins
+          that.setData({
+            point:temp
+          })
+          wx.showToast({
+            title: '签到成功！',
+            icon: 'success',
+            duration: 1200,
+            mask: true
+          })
+        },
+      })
+    }else{
+      wx.showToast({
+        title: '今日打卡已完成！',
+        icon:'loading',
+        duration:1200,
+        mask:true
+      })
+    }
 
   },
-
+  onShow:function(options){
+    var that = this;
+    varcount = wx.getStorage({
+      key: 'FirstTime',
+      success: function (res) {
+        that.setData({
+         count: res.data
+        })
+      },
+    })
+    var D = (new Date()).getDate().toString();
+    if (D != wx.getStorageSync('D')){
+      this.setData({
+        clock_button_flag:true
+      })
+    }else{
+      this.setData({
+        clock_button_flag:false
+      })
+    }
+  },
+  goToTalkPage:function(param){
+    wx.navigateTo({
+      url: '../../pages/score/score01'  
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
@@ -62,5 +130,15 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+  increase_count(){
+    var temp_count=this.data.count
+      if(temp_count<7)
+      temp_count++;
+      else
+      temp_count=0;
+    this.setData({count:temp_count})
+    console.log(temp_count)
+    
   }
 })
